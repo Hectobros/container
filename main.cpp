7901d684b0df87023d628c5a4d7cb5ae40330fde
Niels SCHMITT <nschmitt@e1r12p7.clusters.42paris.fr>
2022-10-01 16:03:34 +0200

    Tout est au propre

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main()
 		std::map<int, int>::iterator f;
 		std::map<int, int>::const_iterator p;
 		f = b.begin();
-		p = b.cbegin();
+		p = b.begin();
 		for(int x = 0; x < 2; x++)
 		{
 			std::cout << f->first << " " << p->first << std::endl;
@@ -47,7 +47,7 @@ int main()
 			p++;
 		}
 		f = b.end();
-		p = b.cend();
+		p = b.end();
 		for(int x = 0; x < 2; x++)
 		{
 			f--;
@@ -65,7 +65,7 @@ int main()
 		std::map<int, int>::reverse_iterator f;
 		std::map<int, int>::const_reverse_iterator p;
 		f = b.rbegin();
-		p = b.crbegin();
+		p = b.rbegin();
 		for(int x = 0; x < 2; x++)
 		{
 			std::cout << f->first << " " << p->first << std::endl;
@@ -73,7 +73,7 @@ int main()
 			p++;
 		}
 		f = b.rend();
-		p = b.crend();
+		p = b.rend();
 		for(int x = 0; x < 2; x++)
 		{
 			f--;
